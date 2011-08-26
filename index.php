f94801e4136624994dfe462f00233b3e16d4e50c
Pete Chen <pete@petechen.com>
2011-08-26 16:28:29 -0700

    this I like
    nothing else to say here.

--- index.php
+++ index.php
@@ -2,18 +2,15 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=MacRoman">
-<title>Insert title here</title>
+<title>semina hummina</title>
 </head>
-<frameset>
-    <frame>
-    <frame>
-    <noframes>
-    <body>
-    <p>This page uses frames. The current browser you are using does not support frames.</p>
-    <?php
 
+    <body>
+    <h1>Have a good time, overtime.</h1>
+    <?php
+	echo "<p>The time today is: ". date("G:i:s") ."</p>";
+	echo "<blockquote>another test: ". strtoupper("many many ways that we can say Hey in Hey in Hey!". "</blockquote>");
 	?>
     </body>
-    </noframes>
-</frameset>
+
 </html>
