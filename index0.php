c433804d985a31b48cd9e577867f0c5e46e1070c
Pete Chen <pete@petechen.com>
2011-08-29 13:24:48 -0700

    merged it

---
+++ index0.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=MacRoman">
+<title>Insert title here</title>
+</head>
+
+
+    <body>
+    <p>This page uses frames. The current browser you are using does not support frames.</p>
+    <?php
+
+	?>
+    </body>
+    </noframes>
+</frameset>
+</html>
